e52f03b823193efb1889f9eff3f231aacdaf5ddd
Viacheslav Iskenderov <iskenderov.vsl@gmail.com>
2025-02-28 15:43:54 +0200

    fix: schema.ts

--- migrations/relations.ts
+++ migrations/relations.ts
@@ -1,29 +1,30 @@
 import { relations } from "drizzle-orm/relations";
-import { usersInAuth, sheets, categories, records } from "./schema";
+import { sheets, categories, records } from "./schema";
+import { authUsers } from "drizzle-orm/supabase";
 
-export const sheetsRelations = relations(sheets, ({one, many}) => ({
-	usersInAuth: one(usersInAuth, {
-		fields: [sheets.userId],
-		references: [usersInAuth.id]
-	}),
-	records: many(records),
+export const sheetsRelations = relations(sheets, ({ one, many }) => ({
+  usersInAuth: one(authUsers, {
+    fields: [sheets.userId],
+    references: [authUsers.id],
+  }),
+  records: many(records),
 }));
 
-export const usersInAuthRelations = relations(usersInAuth, ({many}) => ({
-	sheets: many(sheets),
+export const usersInAuthRelations = relations(authUsers, ({ many }) => ({
+  sheets: many(sheets),
 }));
 
-export const recordsRelations = relations(records, ({one}) => ({
-	category: one(categories, {
-		fields: [records.categoryId],
-		references: [categories.id]
-	}),
-	sheet: one(sheets, {
-		fields: [records.sheetId],
-		references: [sheets.id]
-	}),
+export const recordsRelations = relations(records, ({ one }) => ({
+  category: one(categories, {
+    fields: [records.categoryId],
+    references: [categories.id],
+  }),
+  sheet: one(sheets, {
+    fields: [records.sheetId],
+    references: [sheets.id],
+  }),
 }));
 
-export const categoriesRelations = relations(categories, ({many}) => ({
-	records: many(records),
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  records: many(records),
 }));
