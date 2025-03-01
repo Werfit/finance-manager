d83d15430e44ec252cd84292acf18a6851d45ac5
Viacheslav Iskenderov <iskenderov.vsl@gmail.com>
2025-03-01 17:57:24 +0200

    feat: port env

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 
@@ -54,4 +55,5 @@ def predict_expense():
 
 
 if __name__ == '__main__':
-   app.run(port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
