ba29c0df56593daced93dba00440ff3942972454
Iskenderov Viacheslav <iskenderov.vsl@gmail.com>
2025-03-23 16:08:00 +0200

    feat: removed hydration boundary in order to avoid page timeout on a free tier on vercel

--- src/app/(dashboard)/[id]/page.tsx
+++ src/app/(dashboard)/[id]/page.tsx
@@ -1,14 +1,3 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-
-import {
-  getSheetRecordsQuery,
-  getTotalAmountQuery,
-} from "@/libs/query/queries/records.queries";
-import { getSheetOptions } from "@/libs/query/queries/sheets.queries";
 import { PageProps } from "@/shared/types/layout.type";
 
 import { Dashboard } from "./_components/dashboard/dashboard.component";
@@ -16,20 +5,11 @@ import { SheetNavigation } from "./_components/navigation/sheet-navigation.compo
 
 const Page: React.FC<PageProps<{ id: string }>> = async ({ params }) => {
   const { id } = await params;
-  const queryClient = new QueryClient();
-
-  await Promise.allSettled([
-    queryClient.prefetchInfiniteQuery(getSheetRecordsQuery(id)),
-    queryClient.prefetchQuery(getTotalAmountQuery(id)),
-    queryClient.prefetchQuery(getSheetOptions(id)),
-  ]);
 
   return (
     <div className="mx-auto max-w-2xl">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <SheetNavigation sheetId={id} />
-        <Dashboard sheetId={id} />
-      </HydrationBoundary>
+      <SheetNavigation sheetId={id} />
+      <Dashboard sheetId={id} />
     </div>
   );
 };
